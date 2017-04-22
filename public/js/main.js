65c96b38d4c02ed260853df3116845ee20e7fd98
echen4 <evelynchen97@yahoo.com>
2017-04-22 14:20:57 -0400

    changed buttons to change backgrounds on click

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,8 @@ function myFunction() {
   //$("#demo").html("dogs in space #YEEEHAWWWW");
 
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#doge-image").append(`<img class="img-circle" src="/images/guy fieri.jpg" />`);
+  //$("#doge-image").append(`<img class="img-circle" src="/images/guy fieri.jpg" />`);
+  document.body.style.background = "#f3f3f3 url('/images/guy fieri.jpg') repeat top left fixed";
 }
 
 function newFunction() {
@@ -14,6 +15,7 @@ function newFunction() {
   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
 
   //$("#demo").html("~ cowboy taeyang ~");
-  $("#doge-image").append(`<img class="img-circle" src="/images/lmao.gif" />`);
+  //$("#doge-image").append(`<img class="img-circle" src="/images/lmao.gif" />`);
+  document.body.style.background = "#f3f3f3 url('/images/lmao.gif') repeat top left fixed";
 }
 
