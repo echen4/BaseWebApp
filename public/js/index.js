15de9ffd48ecc2e399a5244bd93e6747e8e3b0f1
echen4 <evelynchen97@yahoo.com>
2017-04-22 13:16:28 -0400

    added nav bar

---
+++ public/js/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// Set up the app's routing
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.get('/about', function(request, response) {
+  response.render('pages/about');
+});
+
+app.get('/projects', function(request, response) {
+  response.render('pages/projects');
+});
+
+app.get('/contact', function(request, response) {
+  response.render('pages/contact');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+// This file is what handles incoming requests and
+// serves files to the browser, or executes server-side code
