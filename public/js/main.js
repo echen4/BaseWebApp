7124adf888af3b2cc66677543aab25c24daf838f
echen4 <evelynchen97@yahoo.com>
2017-04-22 16:14:16 -0400

    changed text

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ function myFunction() {
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
 
-  $("#demo").html("~ shut the front door ~");
+  $(".suffer").html("<h1>~ shut the front door ~</h1>");
 
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
   //$("#doge-image").append(`<img class="img-circle" src="/images/guy fieri.jpg" />`);
@@ -14,7 +14,7 @@ function newFunction() {
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
 
-  $(".suffer").html("~ welcome to flavortown ~");
+  $(".suffer").html("<h1>~ welcome to flavortown ~</h1>");
   //$("#doge-image").append(`<img class="img-circle" src="/images/lmao.gif" />`);
   document.body.style.background = "#f3f3f3 url('/images/guy.jpg') repeat top left fixed";
 }
